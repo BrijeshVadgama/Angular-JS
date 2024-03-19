f79ab2dc6a07841ac00b2dff4b2eae4dc0e0fcd4
Brijesh Vadgama <brijuvadgama@gmail.com>
2024-03-19 18:04:17 +0530

    commit after creating routes

--- routing-demo/src/app/app.module.ts
+++ routing-demo/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductsComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
